0f1838d4df06f218614152787cd377889d8e721f
Alexey Petrakov <petrakan.code@gmail.com>
2019-08-24 20:24:26 +0300

    аукые сщььше

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '965661429:AAFCyLICRezyCFWIX7C7kqkpIjRzETM_-0U'
+
+REQUEST_KWARGS={
+    'proxy_url': 'http://62.141.35.197:3128/',
+}
+
+#updater = Updater(TOKEN, request_kwargs=REQUEST_KWARGS) - подключается так.
