061fe32f407f5d51c044aec0b76b0f0162e76f56
Alexey Petrakov <petrakan.code@gmail.com>
2019-08-25 14:14:41 +0300

    fix

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 TOKEN = '965661429:AAFCyLICRezyCFWIX7C7kqkpIjRzETM_-0U'
 
 REQUEST_KWARGS={
-    'proxy_url': 'http://62.141.35.197:3128/',
+    'proxy_url': 'http://62.141.35.197:3128/', #Адрес proxy, нужен только для локальной разработки из за блокировок РКН
 }
 
-
-#updater = Updater(TOKEN, request_kwargs=REQUEST_KWARGS) - подключается так.
+#updater = Updater(TOKEN, request_kwargs=REQUEST_KWARGS) - подключается в main.py так.
